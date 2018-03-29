1dc5f8fafc4765407f3dd0a3982ae60109ff403b
You Kim <dolzzo@mutecsoft.com>
2018-03-29 18:38:15 +0900

    JAVA 8 compatibility for lamba
    Start next acitivity after 5 sec using Rxjava
    Splash Activity leave no history and exlude from recent.
    Databinding test.

--- app/src/main/java/youkim/rxblesample/SplashActivity.java
+++ app/src/main/java/youkim/rxblesample/SplashActivity.java
@@ -1,13 +1,62 @@
 package youkim.rxblesample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import youkim.rxblesample.databinding.ActivitySplashBinding;
+
+
 public class SplashActivity extends Activity {
+
+    private static final long SPLASH_TIME = 5;
+    private Disposable mStartScan;
+
+    //example how to use databinding.
+    private ActivitySplashBinding mBind;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        mBind = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disposeStartScan();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startActivityDelayed();
+    }
+
+    private synchronized void startActivityDelayed() {
+        if (mStartScan == null || mStartScan.isDisposed()) {
+            mStartScan = Observable.timer(SPLASH_TIME, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .subscribe(time -> startScanActivity());
+        }
+    }
+
+    private synchronized void startScanActivity() {
+        Intent intent = new Intent(getApplicationContext(), DeviceScanActivity.class);
+        startActivity(intent);
+    }
+
+
+    private synchronized void disposeStartScan() {
+        if (mStartScan != null && !mStartScan.isDisposed()) {
+            mStartScan.dispose();
+        }
     }
 }
